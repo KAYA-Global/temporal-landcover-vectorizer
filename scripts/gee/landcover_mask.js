db16dde3b51a73e00e2003bc7c83e50bf5927acb
ulfboge <53839582+ulfboge@users.noreply.github.com>
2025-03-05 09:56:32 +0100

    Update landcover_mask.js
    Mask and images updated to include tree cover and use NDVI images.

--- scripts/gee/landcover_mask.js
+++ scripts/gee/landcover_mask.js
@@ -4,17 +4,17 @@ var polygons = ee.FeatureCollection('projects/ee-komba/assets/kaya/wirong/mask_5
 // Load the WorldCover data and get the first image
 var worldcover = ee.ImageCollection("ESA/WorldCover/v200").first();
 
-// Create a mask for the desired land cover classes (20: Shrubland, 30: Grassland, 60: Bare/Sparse Vegetation)
-var landCoverMask = worldcover.eq(20).or(worldcover.eq(30)).or(worldcover.eq(60));
+// Create a mask for the desired land cover classes (10: Tree Cover, 20: Shrubland, 30: Grassland, 60: Bare/Sparse Vegetation)
+var landCoverMask = worldcover.eq(10).or(worldcover.eq(20)).or(worldcover.eq(30)).or(worldcover.eq(60));
 
 // List of Landsat images to process
 var images = {
-  2013: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2013'),
-  2015: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2015'),
-  2017: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2017'),
-  2019: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2019'),
-  2021: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2021'),
-  2023: ee.Image('projects/ee-komba/assets/kaya/bbox-wirong-Landsat-2023')
+  2013: ee.Image('projects/ee-komba/assets/kaya/NDVI_2013'),
+  2015: ee.Image('projects/ee-komba/assets/kaya/NDVI_2015'),
+  2017: ee.Image('projects/ee-komba/assets/kaya/NDVI_2017'),
+  2019: ee.Image('projects/ee-komba/assets/kaya/NDVI_2019'),
+  2021: ee.Image('projects/ee-komba/assets/kaya/NDVI_2021'),
+  2023: ee.Image('projects/ee-komba/assets/kaya/NDVI_2023')
 };
 
 // Add visualization layers
